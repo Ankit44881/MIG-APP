d76a4d7b4b8fc411427640be5f4c0d8638c9d737
Ankit Raj <ankitraj84097@gmail.com>
2024-10-06 09:23:08 +0000

    error in startup script removed

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y docker.io
-sudo docker run -p 80:80 -d us-central1-docker.pkg.dev/ankit-44881/ankit-repo/hello-world-image:v1
+sudo apt-get update                          # Updates the package index
+sudo apt-get install -y docker.io            # Installs Docker
+gcloud auth configure-docker us-central1-docker.pkg.dev -y  # Configures Docker to use gcloud as a credential helper for your Artifact Registry
+sudo docker run -p 80:80 -d us-central1-docker.pkg.dev/ankit-44881/ankit-repo/hello-world-image:v1  # Runs your Docker container
